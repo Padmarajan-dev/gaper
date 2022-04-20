d359de885ed4f034e0cf3d8ee6f5907bee9cb442
Rajudev <rajussp31121997@gmail.com>
2022-04-20 21:37:04 +0530

    basic drag and drop completed

---
+++ Gaper/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:717ba7a4e3edca4c7b8c0eb857c261bab73f9e3ebc816e67388f461e80f5fc18
+size 1427
---
+++ Gaper/Assets/Scripts/Mirror.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a282a775d7d6af78c001e1f75ff30838004ce84534bfba65d7c6976ed4b5eadf
+size 713
